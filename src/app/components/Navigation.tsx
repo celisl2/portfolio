c72a1ed4969004f3dbc5713e091088f78b11f83b
Laura <lauracelis18@gmail.com>
2024-02-25 14:43:22 -0500

    fixes nav issues

--- src/app/components/Navigation.tsx
+++ src/app/components/Navigation.tsx
@@ -8,9 +8,9 @@ import { useState } from "react";
 type Props = { className: string, isMobile?: boolean, onClick?: any };
 
 function NavigationLinks({ ...props }:Props){
-    let childClassNames = "hover:border-b-2 border-indigo-600";
+    let childClassNames = "hover:border-b-2 border-blue-800";
     if(props.isMobile){
-        childClassNames = "hover:border-b-2 border-indigo-600 flex my-8"
+        childClassNames = "hover:border-b-2 border-indigo-50 flex my-8"
     }
     return  (
         <ul className={props.className}>
@@ -22,33 +22,33 @@ function NavigationLinks({ ...props }:Props){
 }
 
 export default function Nav() {
-    const [showMobileNav, setShowMobileNav] = useState(true);
+    const [showMobileNavIcon, setShowMobileNavIcon] = useState(true);
     const [expandMobileNav, setExpandMobileNav] = useState(false);
 
     function closeMobileNavigation() {
-        setShowMobileNav(true);
+        setShowMobileNavIcon(true);
         setExpandMobileNav(false);
     }
 
     return (
-        <nav className={ expandMobileNav ? "bg-indigo-950" : undefined}>
+        <nav className={ expandMobileNav ? "bg-blue-800 md:bg-transparent" : undefined}>
             <div className="flex sm:flex-wrap md:flex-nowrap justify-between p-12">
-                <Link className={ expandMobileNav ? "hover:border-b-2 border-indigo-600 text-indigo-50 md:flex-none" : "hover:border-b-2 border-indigo-600 md:flex-none"} href="/">LC</Link>
-                { showMobileNav ? (
-                    <VscMenu className="cursor-pointer sm:inline-block md:hidden" onClick={() => { 
-                        setShowMobileNav(false)
+                <Link className={ expandMobileNav ? "hover:border-b-2 border-indigo-50 md:border-blue-800 text-indigo-50 md:text-black md:flex-none" : "hover:border-b-2 border-blue-800 md:text-black md:flex-none"} href="/">LC</Link>
+                { showMobileNavIcon ? (
+                    <VscMenu className="cursor-pointer sm:inline-block md:hidden rounded active:border-2 active:border-blue-800" onClick={() => { 
+                        setShowMobileNavIcon(false)
                         setExpandMobileNav(true)
                     }} size={26}/>
                 ) : (
-                    <VscClose className="cursor-pointer rounded border-2 border-indigo-600 text-indigo-50 sm:inline-block md:hidden" onClick={ () => closeMobileNavigation() } size={27} color="4F46E5"/>
+                    <VscClose className="cursor-pointer rounded active:border-2 active:border-indigo-50 text-indigo-50 sm:inline-block md:hidden" onClick={ () => closeMobileNavigation() } size={27} color="4F46E5"/>
                 )}
-                { showMobileNav ? <NavigationLinks className='hidden space-x-4 sm:hidden md:inline-flex'/>
+                { showMobileNavIcon ? <NavigationLinks className='hidden space-x-4 sm:hidden md:inline-flex'/>
                         : <NavigationLinks className='hidden space-x-4 md:inline-flex' />
                 }
             </div>
 
             { expandMobileNav &&
-                <NavigationLinks className="flex flex-col items-center bg-gray-800 text-indigo-50 p-12 min-h-full md:hidden" isMobile={expandMobileNav} onClick={closeMobileNavigation}/>
+                <NavigationLinks className="flex flex-col items-center bg-blue-800 text-indigo-50 p-12 min-h-full md:hidden" isMobile={expandMobileNav} onClick={closeMobileNavigation}/>
             }
         </nav>
     )
